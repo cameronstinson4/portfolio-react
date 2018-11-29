17c448ff8f654d8465ebc9ea3a240709b7d686d1
Cameron Stinson <cameronstinson4@gmail.com>
2018-11-29 21:05:23 +0000

    hopefully fixed gray text

--- src/text-content.js
+++ src/text-content.js
@@ -2,5 +2,5 @@ module.exports = {
   title: "Cameron Stinson",
   description: "Cameron Stinson\'s portfolio. Full stack software engineer for the information age",
   keywords: "cameron stinson portfolio software engineer",
-  intro: "Hi people!",
+  intro: "Software Engineer",
 }
