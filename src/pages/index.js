45e315729adb3edb604cb44bbce4b12f823dc627
Cameron Stinson <cameronstinson4@gmail.com>
2018-11-30 00:04:23 +0000

    cool transitions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,64 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
+import React, { Component } from "react"
+// components
 import Layout from '../components/layout/layout.js'
-
-import styles from './index.scss'
+// styles
+import styles from './index.module.scss'
 import headerStyles from '../components/header/header.module.scss'
+// content
 import text from '../text-content'
 
-const IndexPage = () => (
-  <Layout largeHeader={true}>
-    <div className={styles.content}>
-      <h1 className={headerStyles.largeText}> {text.intro} </h1>
-    </div>
-  </Layout>
-)
+const textChangeInterval = 5000
+
+class IndexPage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.intros = text.intros;
+    this.index = 0;
+
+    this.state = {
+      intro: this.intros[this.index],
+      faded: false
+    };
+  }
+
+  transitionText = () => {
+
+    this.setState({intro: this.intros[this.index]});
+    this.unFadeText();
+
+    if (this.index < this.intros.length - 1) {
+      this.index += 1;
+    }
+    else {
+      this.index = 0;
+    }
+
+    setTimeout(this.fadeText, textChangeInterval - 500)
+    setTimeout(this.transitionText, textChangeInterval)
+  }
+
+  fadeText = () => {
+    this.setState({faded: true})
+  }
+
+  unFadeText = () => {
+    this.setState({faded: false})
+  }
+
+  componentDidMount = () => {
+
+    this.transitionText()
+
+  }
+
+  render = () => (
+    <Layout largeHeader={true} className={styles.layout}>
+      <div className={styles.content}>
+        <h1 className={`${headerStyles.largeText} ${this.state.faded ? styles.faded : styles.unFaded}` }> {this.state.intro} </h1>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
--- src/text-content.js
+++ src/text-content.js
@@ -2,5 +2,5 @@ module.exports = {
   title: "Cameron Stinson",
   description: "Cameron Stinson\'s portfolio. Full stack software engineer for the information age",
   keywords: "cameron stinson portfolio software engineer",
-  intro: "Software Engineer",
+  intros: ["Software Engineer", "Designer", "Solutions Architect", "Photo\u200Bgrapher"]
 }
